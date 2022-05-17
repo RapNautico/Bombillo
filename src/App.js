7d689cae9ffb6f5666adaa0ee02f80e0eb9f994c
RapNautico <foox299@gmail.com>
2022-05-17 14:41:59 -0500

    Se creo un encerder y apagar con useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useEffect, useState } from "react";
+// import img1 from './../imagenes/prendido.png';
+// import img2 from './../imagenes/apagado.png';
+import apagado from './imagenes/apagado.png';
+import prendido from './imagenes/prendido.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    //luz activa
+    let [activo, setActivo] = useState(false);
+    //contador
+    let [con, setCon] = useState(0);
+    //bombillo
+    let [bom, setBom] = useState(false);
+
+    const encender = ()=>{
+      setActivo(!activo);
+      if(activo === true){
+            setCon(con+1);   
+        }
+        setBom(!bom);
+    }
+   /* useEffect(()=>{
+        setCon(con);
+    },[setCon]);*/
+
+    console.log(activo);
+    console.log(con);
+
+    return(
+      <>
+        <div style={{display: "flex", justifyContent: "center" }}>
+            <img src={bom === true ? apagado : prendido}/>
+        </div>
+        <hr/>
+        <div style={{display: "flex", justifyContent: "center" }}>
+          <p style={{padding: "20px", backgroundColor: "silver"}}>{con}</p>
+          <button style={{ backgroundColor:"green", color:"white", padding: "10px", margin: "10px"}} onClick={encender}> encender </button>
+          <button style={{ backgroundColor:"red", color:"white", padding: "10px", margin: "10px"}} onClick={encender}> apagar </button>
+        </div>
+      </>
+    );
 }
-
-export default App;
